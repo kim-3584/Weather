3827089499c391a42a0e6ada58389ed21c8c479f
kim-3584 <kyrok67@naver.com>
2021-02-24 23:29:39 +0900

    Change search, location Button Size.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
 }
 
