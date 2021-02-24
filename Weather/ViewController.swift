70fa7a8b87aa3c6ef69041f16826de6256520989
kim-3584 <kyrok67@naver.com>
2021-02-24 23:08:34 +0900

    Build UI with Dark mode.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
