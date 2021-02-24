8daaeae9496863bc1cf1a15698b7a3f165ed796b
kim-3584 <kyrok67@naver.com>
2021-02-25 00:08:29 +0900

    Set Search Text Field.

---
+++ Weather/Controller/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by MIN SEONG KIM on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 텍스트필드가 무슨 일이 일어나는지 뷰컨트롤러에 알린다. 텍스트필드가 진행상황을 전달
+        searchTextField.delegate = self
+        
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    // 모든 텍스트 필드에 유효성 검사
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        let city = searchTextField.text
+        cityLabel.text = city
+        searchTextField.text = ""
+    }
+    
+    
+}
+
+
